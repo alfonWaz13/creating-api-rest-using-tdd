68c2c476ae5c39d8b60f54fcccf04b80a0e06a4b
alfonWaz13 <alfonwaz13@icloud.com>
2025-05-03 14:32:56 +0200

    feat: implement create user command handler logic and users repository

--- src/delivery/api/v1/users/users_router.py
+++ src/delivery/api/v1/users/users_router.py
@@ -3,16 +3,18 @@ from http.client import CREATED
 
 from src.delivery.api.v1.users.users_request import CreateUserRequest
 from src.domain.user import User
+from src.infraestructure.in_memory.users_repository import InMemoryUsersRepository
 from src.use_cases.commands.create_user_command import (
     CreateUserCommandHandler,
     CreateUserCommand,
 )
 
 users_router = APIRouter()
+users_repository = InMemoryUsersRepository()
 
 
 def _get_create_user_command_handler() -> CreateUserCommandHandler:
-    return CreateUserCommandHandler()
+    return CreateUserCommandHandler(users_repository)
 
 
 @users_router.post("/", status_code=CREATED)
---
+++ src/domain/users_repository.py
@@ -0,0 +1,8 @@
+from abc import ABC, abstractmethod
+
+from src.domain.user import User
+
+
+class UsersRepository(ABC):
+    @abstractmethod
+    def save(self, user: User) -> None: ...
